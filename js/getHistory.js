b024020c4f3fd0972c4a1143ef89b838ae92176e
Michelle Li <michellecarolynclark@gmail.com>
2018-03-18 09:18:20 -0700

    history

--- js/getHistory.js
+++ js/getHistory.js
@@ -23,10 +23,36 @@ $.ajax({
     dataType: "json"
 }).done(function(json) {
     // Handle Response
-     for(i = json.length - 1; i > json.length - 5; i++) {
+    if (json.length == 0) {
+        $("#no-hist").attr('style','visibility: visible');
+    }
+    var counter = 0;
+    if (json.length < 2) {
+        $("#picture2").attr('style','visibility: visible');
+    }
+    if (json.length < 3) {
+        $("#picture3").attr('style','visibility: visible');
+    }
+    if (json.length < 4) {
+        $("#picture4").attr('style','visibility: visible');
+    }
+     for (i = json.length - 1; i > json.length - 5; i--) {
         var url = "https://filestore.blurriness60.hasura-app.io/v1/file/";
          url += json.file_id;
-         processImage(url);
+        // renderImage(url, counter);
+         if (counter == 0) {
+             document.querySelector("#picture1").src = url;
+         }
+         if (counter == 1) {
+             document.querySelector("#picture2").src = url;
+         }
+         if (counter == 2) {
+             document.querySelector("#picture3").src = url;
+         }
+         if (counter == 3) {
+             document.querySelector("#picture4").src = url;
+         }
+         counter++;
      }
 
 }).fail(function(xhr, status, errorThrown) {
@@ -34,43 +60,3 @@ $.ajax({
     console.log("Status: " + status);
     console.dir(xhr);
 });
-// var fetchAction =  require('fetch');
-
-// var url = "https://data.blurriness60.hasura-app.io/v1/query";
-
-// // If you have the auth token saved in offline storage
-// // var authToken = window.localStorage.getItem('HASURA_AUTH_TOKEN');
-// // headers = { "Authorization" : "Bearer " + authToken }
-// var requestOptions = {
-//     "method": "POST",
-//     "headers": {
-//         "Content-Type": "application/json",
-//         "Authorization": "Bearer f533a6e4ab6d6787f9375a540cae4b85084297f455e82f77"
-//     }
-// };
-
-// var body = {
-//     "type": "select",
-//     "args": {
-//         "table": {
-//             "schema": "hf_catalog",
-//             "name": "hf_file"
-//         },
-//         "columns": [
-//             "*"
-//         ]
-//     }
-// };
-
-// requestOptions.body = JSON.stringify(body);
-
-// fetchAction(url, requestOptions)
-// .then(function(response) {
-// 	return response.json();
-// })
-// .then(function(result) {
-// 	console.log(result);
-// })
-// .catch(function(error) {
-// 	console.log('Request Failed:' + error);
-// });
