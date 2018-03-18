3b29648bf5f2238e9f2e5c6ba92e0f103b7053b3
Michelle Li <michellecarolynclark@gmail.com>
2018-03-18 07:34:35 -0700

    add descriptive audio

--- js/main.js
+++ js/main.js
@@ -51,6 +51,7 @@ function getDescription(url) {
             //var dataS = JSON.stringify(data);
 
             var msg = new SpeechSynthesisUtterance(label);
+            window.speechSynthesis.speak(new SpeechSynthesisUtterance("Image Description"));
             window.speechSynthesis.speak(msg);
         })
 
@@ -124,9 +125,13 @@ function getImageText(url) {
             $("#textTextArea").val(text);
 
             //var dataS = JSON.stringify(data);
-
-            var msg = new SpeechSynthesisUtterance(text);
-            window.speechSynthesis.speak(msg);
+            if (text.length() > 0) {
+                var msg = new SpeechSynthesisUtterance(text);
+                window.speechSynthesis.speak(msg);
+            } else {
+                var msg = new SpeechSynthesisUtterance("There are no words in this image.");
+                window.speechSynthesis.speak(msg);
+            }
         })
 
         .fail(function(jqXHR, textStatus, errorThrown) {
